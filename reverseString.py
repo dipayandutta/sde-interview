ddb012d2e1f57a3975fb3d1d8178fa0025da6819
Dipayan Dutta <dipayandutta@Dipayans-MacBook-Air.local>
2026-01-01 20:39:54 +0530

    Reverse String in Python

---
+++ reverseString.py
@@ -0,0 +1,10 @@
+'''
+Program to Reverse a string
+'''
+def reverseString(string):
+	return string[::-1]
+
+string="Hello"
+result = reverseString(string)
+print(f"Main String -->  {string}")
+print(f"Reverse String --> {result}")
