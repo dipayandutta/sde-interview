85cf8e009f62e5c660ea0537b7205a554273abf6
Dipayan Dutta <dipayandutta@Dipayans-MacBook-Air.local>
2026-01-01 21:48:55 +0530

    Problem - Print the First Non Repeating Character

---
+++ firstNonRepeatingChar.py
@@ -0,0 +1,29 @@
+'''
+Question - First Non-Repeating Characte
+Return the first character that does not repeat.
+Input:
+"swiss"
+Output:
+"w"
+'''
+'''
+|=============|
+|   #LOGIC    |
+|=============|
+Count frequency of each character
+Iterate original string
+Return first character with count == 1
+'''
+
+def first_non_repeating_char(string):
+	freq = {}
+	for char in string:
+		freq[char] = freq.get(char,0)+1
+
+	for char in string:
+		if freq[char] == 1:
+			return char
+	return None
+
+string="swiss"
+print(first_non_repeating_char(string))
