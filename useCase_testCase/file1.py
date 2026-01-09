7909c3b0524cec2af0157c1aeb25ea01ab5b626e
Dipayan Dutta <dipayandutta@Dipayans-MacBook-Air.local>
2026-01-10 01:52:48 +0530

    What is the Use of the if __name__ == '__main__'

---
+++ useCase_testCase/file1.py
@@ -0,0 +1,11 @@
+# use Case Code
+
+def add(a,b):
+	return a+b
+
+# Test Case Code
+if __name__ == '__main__':
+	if add(1,2) ==3:
+		print("Pass")
+	else:
+		print("False")
---
+++ useCase_testCase/file2.py
@@ -0,0 +1,3 @@
+import file1
+
+print(file1.add(1,2))
