c48878a7069222a0e2b89c572050cb98e02cc19c
Dipayan Dutta <inbox.dipayan@gmail.com>
2026-03-08 12:30:45 +0530

    normal vs yield

--- yield/normal_vs_yield.py
+++ yield/normal_vs_yield.py
@@ -1,11 +1,20 @@
 for i in range(1,3):
 	print(i)  # stores in a memory at once
 
+print("Yield Output")
+print("------------")
+
+
 def simple_yield():
 	for i in range(1,3):
 		yield i
+print(next(simple_yield()))
+print(next(simple_yield()))
 
+'''
 result = simple_yield()
 
 for data in result:
 	print(data)
+
+'''
