f83ede8bd35f79b90069df8aa9314f1522096c0c
Dipayan Dutta <inbox.dipayan@gmail.com>
2026-03-24 00:29:15 +0530

    pytest for class Example

---
+++ pytest/ExampleOfClass/test_main.py
@@ -0,0 +1,16 @@
+import pytest 
+from main import UserManagment
+
+@pytest.fixture
+def user_manager():
+    return UserManagment()
+
+
+def test_add_user(user_manager):
+    assert user_manager.add_user("john_doe", "john@example.com") == True
+    assert user_manager.get_user("john_doe") == "john@example.com"
+
+def test_add_duplicate_user(user_manager):
+    user_manager.add_user("john_doe","john@example.com")
+    with pytest.raises(ValueError):
+        user_manager.add_user("john_doe","john@example.com")
