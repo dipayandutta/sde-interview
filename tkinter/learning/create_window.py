36ccb8328274f91969de5f2685beee353debe4a8
Dipayan Dutta <inbox.dipayan@gmail.com>
2026-04-15 00:28:54 +0530

    Python Tkinter Application to create a window

---
+++ tkinter/learning/create_window.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+# Create Buttons
+button1 = Button(root, text="click Me")
+button2 = Button(root, text="Click Us")
+
+button1.pack()
+button2.pack()
+
+root.mainloop()
